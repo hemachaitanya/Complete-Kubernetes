3793863e23ca7ed96aab24a890a5164f567a2574
shiva <sj@gmail.com>
2024-01-16 15:57:00 +0530

    added scripting

--- rbac/certificate.sh
+++ rbac/certificate.sh
@@ -71,3 +71,7 @@ kubectl config use-context user1-context
 kubectl create namespace ns-test
 
 kubectl get pods
+
+ openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout tls.key -out tls.crt -subj "/CN=nginxsvc/O=nginxsvc"
+ kubectl create secret tls tls-secret --key tls.key --cert tls.crt
+
